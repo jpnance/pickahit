6c55e41c671d6edca4209917d4d3d75d35ff9d82
Patrick Nance <jpnance@gmail.com>
2017-09-30 15:34:59 -0700

    Making the big board controller method rely on Session.withActiveSession().

--- services/games.js
+++ services/games.js
@@ -3,39 +3,27 @@ var User = require('../models/User');
 var Game = require('../models/Game');
 
 module.exports.showAll = function(request, response) {
-	var data = [
-		User.find({}).sort({ username: 1 }),
-		Game.find({}).sort({ startTime: 1 }).populate('away.team').populate('home.team')
-	];
+	Session.withActiveSession(request, function(error, session) {
+		var data = [
+			User.find({}).sort({ username: 1 }),
+			Game.find({}).sort({ startTime: 1 }).populate('away.team').populate('home.team')
+		];
 
-	Promise.all(data).then(function(values) {
-		var responseData = {
-			users: values[0].filter(function(user) {
-				return user.isEligibleFor(2017);
-			}),
-			games: values[1],
-			dateFormat: require('dateformat')
-		};
+		Promise.all(data).then(function(values) {
+			var responseData = {
+				users: values[0].filter(function(user) {
+					return user.isEligibleFor(2017);
+				}),
+				games: values[1],
+				dateFormat: require('dateformat')
+			};
 
-		if (request.cookies.sessionId) {
-			Session.find({
-				_id: request.cookies.sessionId
-			}).populate('user').exec(function(error, sessions) {
-				if (error) {
-					response.send(error);
-				}
-				else if (sessions.length == 1) {
-					responseData.session = sessions[0];
-					response.render('index', responseData);
-				}
-				else {
-					response.render('index', responseData);
-				}
-			});
-		}
-		else {
+			if (session) {
+				responseData.session = session;
+			}
+
 			response.render('index', responseData);
-		}
+		});
 	});
 };
 
