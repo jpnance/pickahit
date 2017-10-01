a9ddde5eb300e6ac3415ee36f70184c476b34172
Patrick Nance <jpnance@gmail.com>
2017-09-30 20:09:57 -0700

    Might be useful to have this test script committed.

--- bin/test.js
+++ bin/test.js
@@ -1,16 +1,21 @@
 var User = require('../models/User');
 var Game = require('../models/Game');
+var Player = require('../models/Player');
 
 var mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI);
 
 var data = [
-	User.find({}),
-	Game.find({}).sort({ startTime: 1 }).populate('awayTeam').populate('homeTeam')
+	Game
+		.findById(492508)
+		.populate('picks.player')
+		.populate('picks.user')
 ];
 
 Promise.all(data).then(function(values) {
-	console.log(values);
+	var game = values[0];
+
+	console.log(game.picks);
 	mongoose.disconnect();
 	process.exit();
 });
