3774bb53913e4a1513761f300881c808143748c6
Patrick Nance <jpnance@gmail.com>
2018-10-05 13:41:10 -0700

    Trivial API update to try to trigger Heroku black magic.

--- bin/games.js
+++ bin/games.js
@@ -12,7 +12,7 @@ Game.find({}).sort('startTime').exec(function(error, games) {
 
 	games.forEach(function(game) {
 		gamePromises.push(new Promise(function(resolve, reject) {
-			request.get('https://statsapi.mlb.com/api/v1/game/' + game._id + '/feed/live', function(error, response) {
+			request.get('https://statsapi.mlb.com/api/v1.1/game/' + game._id + '/feed/live', function(error, response) {
 				var data = JSON.parse(response.text);
 
 				if (!data.liveData || !data.liveData.boxscore || !data.liveData.boxscore.teams) {
