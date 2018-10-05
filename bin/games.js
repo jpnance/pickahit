ced8171d235a507513f920240d8d94bf7e823598
Patrick Nance <jpnance@gmail.com>
2018-10-05 14:20:20 -0700

    Rolling back to v1--whoops--and making a sane change.

--- bin/games.js
+++ bin/games.js
@@ -7,12 +7,12 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
-Game.find({}).sort('startTime').exec(function(error, games) {
+Game.find({ season: process.env.SEASON }).sort('startTime').exec(function(error, games) {
 	var gamePromises = [];
 
 	games.forEach(function(game) {
 		gamePromises.push(new Promise(function(resolve, reject) {
-			request.get('https://statsapi.mlb.com/api/v1.1/game/' + game._id + '/feed/live', function(error, response) {
+			request.get('https://statsapi.mlb.com/api/v1/game/' + game._id + '/feed/live', function(error, response) {
 				var data = JSON.parse(response.text);
 
 				if (!data.liveData || !data.liveData.boxscore || !data.liveData.boxscore.teams) {
@@ -23,82 +23,54 @@ Game.find({}).sort('startTime').exec(function(error, games) {
 				var awayTeam = data.liveData.boxscore.teams.away;
 				var homeTeam = data.liveData.boxscore.teams.home;
 
-				Object.keys(awayTeam.players).forEach(function(key) {
-					var player = awayTeam.players[key];
+				[ { team: awayTeam, name: 'away' }, { team: homeTeam, name: 'home' } ].forEach(tuple => {
+					var team = tuple.team;
+					var name = tuple.name;
 
-					if (player.batterPitcher) {
-						var playerId = parseInt(player.id);
+					Object.keys(team.players).forEach(function(key) {
+						var player = team.players[key];
 
-						if (player.batterPitcher == 'p') {
-							if (game.away.pitchers.indexOf(playerId) == -1) {
-								game.away.pitchers.push(playerId);
+						if (player.batterPitcher) {
+							var playerId = parseInt(player.id);
+
+							if (player.batterPitcher == 'p') {
+								if (game[name].pitchers.indexOf(playerId) == -1) {
+									game[name].pitchers.push(playerId);
+								}
 							}
-						}
-						else if (player.batterPitcher == 'b') {
-							if (game.away.batters.indexOf(playerId) == -1) {
-								game.away.batters.push(playerId);
-							}
+							else if (player.batterPitcher == 'b') {
+								if (game[name].batters.indexOf(playerId) == -1) {
+									game[name].batters.push(playerId);
+								}
 
-							if (parseInt(player.gameStats.batting.hits) > 0) {
-								if (game.hits.indexOf(playerId) == -1) {
-									game.hits.push(playerId);
+								if (parseInt(player.gameStats.batting.hits) > 0) {
+									if (game.hits.indexOf(playerId) == -1) {
+										game.hits.push(playerId);
+									}
 								}
 							}
 						}
-					}
-					else if (player.person) {
-						var playerId = parseInt(player.person.id);
+						else if (player.person) {
+							var playerId = parseInt(player.person.id);
 
-						if (player.position.code == '1') {
-							if (game.away.pitchers.indexOf(playerId) == -1) {
-								game.away.pitchers.push(playerId);
+							if (player.position.code == '1') {
+								if (game[name].pitchers.indexOf(playerId) == -1) {
+									game[name].pitchers.push(playerId);
+								}
 							}
-						}
-						else if (player.position.code != '1') {
-							if (game.away.batters.indexOf(playerId) == -1) {
-								game.away.batters.push(playerId);
-							}
-						}
-					}
-				});
+							else if (player.position.code != '1') {
+								if (game[name].batters.indexOf(playerId) == -1) {
+									game[name].batters.push(playerId);
+								}
 
-				Object.keys(homeTeam.players).forEach(function(key) {
-					var player = homeTeam.players[key];
-
-					if (player.batterPitcher) {
-						var playerId = parseInt(player.id);
-
-						if (player.batterPitcher == 'p') {
-							if (game.home.pitchers.indexOf(playerId) == -1) {
-								game.home.pitchers.push(playerId);
-							}
-						}
-						else if (player.batterPitcher == 'b') {
-							if (game.home.batters.indexOf(playerId) == -1) {
-								game.home.batters.push(playerId);
-							}
-
-							if (parseInt(player.gameStats.batting.hits) > 0) {
-								if (game.hits.indexOf(playerId) == -1) {
-									game.hits.push(playerId);
+								if (parseInt(player.stats.batting.hits) > 0) {
+									if (game.hits.indexOf(playerId) == -1) {
+										game.hits.push(playerId);
+									}
 								}
 							}
 						}
-					}
-					else if (player.person) {
-						var playerId = parseInt(player.person.id);
-
-						if (player.position.code == '1') {
-							if (game.home.pitchers.indexOf(playerId) == -1) {
-								game.home.pitchers.push(playerId);
-							}
-						}
-						else if (player.position.code != '1') {
-							if (game.home.batters.indexOf(playerId) == -1) {
-								game.home.batters.push(playerId);
-							}
-						}
-					}
+					});
 				});
 
 				if (data.gameData.probablePitchers) {
