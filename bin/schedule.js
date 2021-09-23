973bc2249eeee1142c000ceea80f578f485cfc5e
Patrick Nance <jpnance@gmail.com>
2021-09-22 17:55:10 -0700

    Revert these point values.

--- bin/schedule.js
+++ bin/schedule.js
@@ -62,15 +62,15 @@ for (var i = 0; i <= days; i++) {
 							break;
 
 						case 'Division Series':
-							newGame.points = 2;
+							newGame.points = 1;
 							break;
 
 						case 'League Championship Series':
-							newGame.points = 4;
+							newGame.points = 2;
 							break;
 
 						case 'World Series':
-							newGame.points = 8;
+							newGame.points = 4;
 							break;
 
 						default: break;
