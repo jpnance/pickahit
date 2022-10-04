c85ace5a7da3918bf5f5a29bcde0333afc4fd01e
Patrick Nance <jpnance@gmail.com>
2022-10-04 13:02:34 -0700

    Standardizing the dotenv line across the application.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var dotenv = require('dotenv').config();
+var dotenv = require('dotenv').config({ path: '/app/.env' });
 
 var express = require('express');
 var bodyParser = require('body-parser');
