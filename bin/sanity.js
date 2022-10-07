1d8c1e6f373d84a2937588adb81d93240f9e4174
Patrick Nance <jpnance@gmail.com>
2022-10-06 22:20:55 -0700

    This sanity script is slightly more useful (but only slightly).

--- bin/sanity.js
+++ bin/sanity.js
@@ -1,3 +1,5 @@
+var dotenv = require('dotenv').config({ path: '/app/.env' });
+
 var User = require('../models/User');
 var Game = require('../models/Game');
 
@@ -5,17 +7,13 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGODB_URI);
 
-Game.find({ season: process.env.SEASON }).sort('startTime').then(games => {
-	games.forEach(game => {
-		var users = [];
-
+Game.find({ season: process.env.SEASON }).sort('startTime').then((games) => {
+	games.forEach((game) => {
 		if (game.picks.length) {
-			game.picks.forEach(pick => {
-				if (users.includes(pick.user.toString())) {
-					console.log('ALERT', game._id, pick.user);
+			game.picks.forEach((pick) => {
+				if (!game.away.batters?.includes(pick.player) && !game.home.batters?.includes(pick.player)) {
+					console.log('ALERT', game._id, pick.player);
 				}
-
-				users.push(pick.user.toString());
 			});
 		}
 	});
